f995360848d2f3e6544e85c03e9bc1ae9ba8508a
manar bahy eldeen <112812979+mannarbahy@users.noreply.github.com>
2022-12-21 18:23:44 +0200

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,32 @@
+function clicklinkedin(){
+    window.open("https://www.linkedin.com/in/manar-bahy-899759208/", "_blank");
+}
+function clickgithub(){
+    window.open("https://github.com/mannarbahy", "_blank");
+}
+function clicktelegram(){
+    window.open("https://t.me/Manarbahy", "_blank");
+}
+
+function clickemail(){
+    window.open("manar.bahyy@gmail.com", "_blank");
+}
+function clickfacebook(){
+    window.open("https://www.facebook.com/manar.kadr/", "_blank");
+}
+function clickinstagram(){
+    window.open("https://www.instagram.com/manar.pahy/?next=%2F", "_blank");
+}
+function clickcodepen(){
+    window.open("https://codepen.io/mannarbahy", "_blank");
+}
+function clicktwitter(){
+    window.open("https://twitter.com/manar_bahy", "_blank");
+}
+function web(){
+    window.location.href = "front.html";
+}
+function back_end(){
+    window.location.href = 'back_end.html';
+}
+
